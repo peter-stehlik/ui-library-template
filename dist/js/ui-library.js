823d86a4a56c97bf76b8eb8768c0191bf7e78d35
Peter Stehlik <peter@inovative.sk>
2020-10-27 13:51:44 +0100

    prepare raw HMTL structure

--- dist/js/ui-library.js
+++ dist/js/ui-library.js
@@ -1,1 +1,17 @@
 "use strict";
+
+var SideNav = {
+  toggleSideNav: function toggleSideNav($this) {
+    var $switcher = $this;
+    var $side = document.querySelector(".grid-side");
+    $switcher.classList.toggle("is-active");
+    $side.classList.toggle("is-opened");
+  }
+};
+var $sideSwitcher = document.getElementById("sideSwitcher");
+
+if ($sideSwitcher) {
+  $sideSwitcher.addEventListener("click", function () {
+    SideNav.toggleSideNav(this);
+  });
+}
